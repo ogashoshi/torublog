e43c4de8823942ebbd622559570963e4ba1859a6
Toru Ogasawara <ujina@me.com>
2022-07-30 17:38:15 +0900

    commit

--- docs/blog/.vuepress/config.js
+++ docs/blog/.vuepress/config.js
@@ -7,6 +7,14 @@ module.exports = {
     plugins: [
         '@vuepress/blog',
       ]
+    },
+    locales: {
+      // The key is the path for the locale to be nested under.
+      // As a special case, the default locale can use '/' as its path.
+      '/': {
+        lang: 'ja-JP', // this will be set as the lang attribute on <html>
+      
+      }
     }
 
   }
