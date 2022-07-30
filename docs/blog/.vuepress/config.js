7a9ebb154085a3629d2d2c0c4c2d43799c1d6a09
Toru Ogasawara <ujina@me.com>
2022-07-30 15:04:20 +0900

    first commit

---
+++ docs/blog/.vuepress/config.js
@@ -0,0 +1,14 @@
+
+module.exports = {
+    title: 'TORU BlOG ', // Title for the site. This will be displayed in the navbar.
+    theme: '@vuepress/theme-blog',
+    themeConfig: {
+      // Please keep looking down to see the available options.
+    plugins: [
+        '@vuepress/blog',
+      ]
+    }
+
+  }
+
+  
